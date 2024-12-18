3d708f954e6e9afe54c0fac4777f58e4dd41080d
iamnotloser <1689986307@qq.com>
2024-12-18 20:44:12 +0800

    se commit

---
+++ src/main/java/com/yupi/springbootinit/service/ChartService.java
@@ -0,0 +1,13 @@
+package com.yupi.springbootinit.service;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author leeyou
+* @description 针对表【chart(图表信息表)】的数据库操作Service
+* @createDate 2024-12-18 20:37:14
+*/
+public interface ChartService extends IService<Chart> {
+
+}
